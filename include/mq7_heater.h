0809c9f64bf9a94afbce699e18d73364a0e74d7a
Magnus3327 <matteo.miglio2709@gmail.com>
2026-01-29 17:45:31 +0100

    update init

--- include/mq7_heater.h
+++
@@ -1,8 +0,0 @@
-// include/mq7_heater.h
-#pragma once
-#include <stdbool.h>
-
-void mq7_heater_init(void);
-void mq7_heater_set(bool on);
-bool mq7_heater_get(void);
-
--- include/mq_adc.h
+++
@@ -1,7 +0,0 @@
-// include/mq_adc.h
-#pragma once
-#include <stdint.h>
-
-void mq_adc_init(void);
-uint16_t mq_adc_read_raw(uint8_t adc_channel);
-
