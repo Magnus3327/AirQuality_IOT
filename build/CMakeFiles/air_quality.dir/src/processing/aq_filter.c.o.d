bec59e3126d13f209dc609f0df1b2a77fc2587d6
Magnus3327 <matteo.miglio2709@gmail.com>
2026-01-30 11:31:47 +0100

    build test

---
+++ build/CMakeFiles/air_quality.dir/src/processing/aq_filter.c.o.d
@@ -0,0 +1,12 @@
+CMakeFiles/air_quality.dir/src/processing/aq_filter.c.o: \
+ /Users/matteo/Università/UNI2026/IOT/project/AirQuality_IOT/src/processing/aq_filter.c \
+ /Users/matteo/Università/UNI2026/IOT/project/AirQuality_IOT/src/processing/aq_filter.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /Users/matteo/Università/UNI2026/IOT/project/AirQuality_IOT/src/config/app_config.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdbool.h
